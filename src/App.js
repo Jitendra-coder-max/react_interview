13137b6e7e5cbdc2111c115386b2517f49aa57b9
Jitendra <nishadji1245@gmail.com>
2024-03-18 14:05:53 -0400

    pagination file added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,193 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+// import Dropdown from './Dropdown';
+// function App() {
 
-function App() {
+
+//   const users = [
+//     { name: "John Doe", id: 1 },
+//     { name: "Jane Doe", id: 2 },
+//     { name: "Billy Doe", id: 3 }
+//   ];
+
+//   return (
+//     <div className="App">
+//      <h1>Hello</h1>
+
+//      <Dropdown  users ={users} />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+//  import React from 'react';
+
+// import Toggle from './Toggle'
+// import Togglecolor from './Togglecolor';
+
+// import Togglehover from './Togglehover';
+
+// import Binding from './Binding';
+
+// import Parent from './Parent';
+
+// import Sumtwo from './Sumtwo';
+
+// import Counter from './Counter';
+
+// import Fetch from './Fetch';
+//  const App = () => {
+
+// return (
+
+//   <>  <h1> hello</h1>
+
+//   <Fetch />
+//   {/* < Togglecolor /> */}
+//   {/* <Toggle /> */}
+// {/* <Binding /> */}
+//   {/* <Togglehover /> */}
+
+//   {/* <Parent /> */}
+
+//   {/* <Sumtwo /> */}
+
+//   {/* <Counter /> */}
+// </>
+// )
+  
+//  }
+
+//  export default App;
+
+
+// import React from 'react'
+// import Fetchdata from './components/Fetchdata'
+// import Number from './components/Number'
+// import Count from './components/Count'
+
+//  import   Parent from './Parent'
+// const App = () => {
+//   return (
+//     <div>App
+//       {/* <Fetchdata/> */}
+//       {/* <Number /> */}
+//       {/*  <Count />  */}
+
+//        <Parent />
+
+
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+// import React from 'react'
+// import {Add} from './components/Add'
+// const App = () => {
+//   return (
+//     <div>App
+
+//     <Add />
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+// import React from 'react'
+// import { Detailsarr } from './components/Detailsarr'
+// const App = () => {
+//   return (
+//     <div>App
+//     <Detailsarr />
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+// import React from 'react'
+// import Formdata from './components/Formdata'
+// const App = () => {
+//   return (
+//     <div>App
+    
+//     <Formdata />
+    
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+// import React from 'react'
+// import Formimp from './components/Formimp'
+// const App = () => {
+//   return (
+//     <div>App
+//       <Formimp />
+//       </div>
+//   )
+// }
+
+// export default App
+
+// import React from 'react'
+// import Uncontrolled from './components/Uncontrolled'
+// const App = () => {
+//   return (
+//     <div>App
+//     <Uncontrolled />
+//     </div>
+//   )
+// }
+
+// export default App
+
+// import React from 'react'
+// import InnerH from './components/InnerH'
+
+// const App = () => {
+//   return (
+//     <div>App
+//     <InnerH />
+//     </div>
+//   )
+// }
+
+// export default App
+
+// import React from 'react'
+// import Fetchapi from './apirequest/Fetchapi';
+// const App = () => {
+//   return (
+//     <div>App
+//     <Fetchapi /></div>
+//   )
+// }
+
+// export default App
+
+// 
+
+
+import React from 'react'
+import Pagination from './pagination/Pagination'
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <Pagination />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
